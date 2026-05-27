f8184e4b13c7b1a8a7cda7378a069554741b7786
yalejong96-bit <yalejong96@gmail.com>
2026-05-27 18:12:10 +0900

    admin 계정 관리1

--- src/main/java/com/moida/config/DataInitializer.java
+++ src/main/java/com/moida/config/DataInitializer.java
@@ -9,6 +9,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+
 @Slf4j
 @Component
 @RequiredArgsConstructor
